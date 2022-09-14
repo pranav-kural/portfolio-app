9dc2ffafcd15c56932dc398d5098acd8bb0c0711
pranav-kural <pranavkural042@gmail.com>
2022-09-14 18:17:19 -0400

    #2 implemented code to use next/image optimization

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
+
 export default function AboutComponent() {
   return (
     <div id="about" uk-grid="true">
       <div id="aboutMeBackground" className="uk-width-2-5">
-        <div>
-          <div>
-            <div></div>
+        <div className="landingImageContainer">
+          <div className="layer1">
+            <Image
+              src="/assets/images/landing-cover.jpg"
+              alt="sample"
+              layout="responsive"
+              width="100%"
+              height="100%"
+              priority={true}
+            />
           </div>
+          <div className="layer2"></div>
+          <div className="layer3"></div>
+          <div className="layer4"></div>
         </div>
       </div>
       <div
---
+++ components/skills.tsx
@@ -0,0 +1,31 @@
+export default function SkillsComponent() {
+  return (
+    <div>
+      <h2>Skills</h2>
+      <ul>
+        <li>
+          Provide Tier I and II level support for triaging and initiating
+          incident resolution for major systems and applications related issues
+          affecting the bank on a medium to large scale
+        </li>
+        <li>
+          Monitor performance and availability of systems, applications and
+          links to ensure full bank functionality across North America
+        </li>
+        <li>
+          Performing initial investigation for any production batch failures
+          through the SDSF, ISPF, InfoPac and server log analysis
+        </li>
+        <li>
+          Assisting in gathering and coordination of proper technical resources
+          in an event of a production issue and ensuring batch/report SLAs are
+          being met
+        </li>
+        <li>
+          Participating as a Mainframe Applications Analyst in enterprise-wide
+          Disaster Recovery exercises and IPLs
+        </li>
+      </ul>
+    </div>
+  );
+}
